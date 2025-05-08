d715f63391a7a61b21b5171fb2ff3ecc96dbfb00
areyes2853 <46231827+areyes2853@users.noreply.github.com>
2025-05-08 16:12:20 -0400

    better set up

--- Todolist.js
+++
@@ -1,6 +0,0 @@
-function greeter(person) {
-    return "By, " + person.firstName + " " + person.lastName;
-}
-var user = { firstName: 'Angel', lastName: 'Reyes' };
-var greetingElement = document.getElementById('greeting-output');
-greetingElement.textContent = greeter(user);
